f957edbd779cf9e64b89553dcc1ab4e4a2285126
alfierilucapio <alfierilucapio@gmail.com>
2024-09-18 17:10:12 +0200

    Cambio nomi animazioni

--- assets/js/ScrollReveal.js
+++ assets/js/ScrollReveal.js
@@ -1,3 +1,5 @@
 ScrollReveal({distance:'100px'},{delay:500},{duration:'1100'},{easing:'cubic-bezier(.215, .61, .355, 1)'});
-ScrollReveal().reveal('.certificate-card-left',{origin:'left',reset:'true'});
-ScrollReveal().reveal('.certificate-card-right',{origin:'right',reset:'true'});
+ScrollReveal().reveal('.reveal-left',{origin:'left',reset:'true'});
+ScrollReveal().reveal('.reveal-right',{origin:'right',reset:'true'});
+ScrollReveal().reveal('.reveal-top',{origin:'top', reset:'true'});
+ScrollReveal().reveal('.reveal-bottom',{origin:'bottom', reset:'true'});
