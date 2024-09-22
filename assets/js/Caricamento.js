5e185be5a43170248e0bfbd385c64dd76670ea91
alfierilucapio <146241829+alfierilucapio@users.noreply.github.com>
2024-09-22 20:31:56 +0200

    Update Caricamento.js

--- assets/js/Caricamento.js
+++ assets/js/Caricamento.js
@@ -1,18 +1,7 @@
-let isPageLoaded = false;
-let isVideoEnded = false;
+const video = document.getElementById('video_caricamento');
+const mainContent = document.getElementById('main-content');
 
-function hideLoader() {
-    if (isPageLoaded && isVideoEnded) {
-        document.getElementById('schermata_caricamento').style.display = 'none';
-    }
-}
-
-window.addEventListener('load', function() {
-    isPageLoaded = true;
-    hideLoader();
-});
-
-document.getElementById('video_caricamento').addEventListener('ended', function() {
-    isVideoEnded = true;
-    hideLoader();
+video.addEventListener('ended', () => {
+  mainContent.style.display = 'block';
+  video.style.display = 'none';
 });
