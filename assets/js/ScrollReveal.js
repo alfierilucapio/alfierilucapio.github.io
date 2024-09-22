4d2874d3e36c65277ac70d599f22791461c5b8f7
alfierilucapio <alfierilucapio@gmail.com>
2024-09-22 18:52:32 +0200

    Regolazione animazioni

--- assets/js/ScrollReveal.js
+++ assets/js/ScrollReveal.js
@@ -1,6 +1,7 @@
-ScrollReveal({distance:'100px',delay:500,duration:'2000',easing:'cubic-bezier(.215, .61, .355, 1)'});
+ScrollReveal({distance:'100px',delay:100,duration:'1100',easing:'cubic-bezier(.215, .61, .355, 1)'});
 
 ScrollReveal().reveal('.reveal-zoom',{scale:1.2, reset:'true'})
+
 ScrollReveal().reveal('.reveal-left',{origin:'left',reset:'true'});
 ScrollReveal().reveal('.reveal-right',{origin:'right',reset:'true'});
 ScrollReveal().reveal('.reveal-top',{origin:'top', reset:'true'});
