15c40a5ed658be7b221ab775b5c45e798d356889
alfierilucapio <alfierilucapio@gmail.com>
2024-09-22 20:01:04 +0200

    prova video

--- assets/js/Caricamento.js
+++ assets/js/Caricamento.js
@@ -1,5 +1,21 @@
-window.addEventListener(
-    'load', function() {
+let isPageLoaded = false;
+let isVideoEnded = false;
+
+// Imposta isPageLoaded a true quando la pagina è completamente caricata
+window.addEventListener('load', function() {
+    isPageLoaded = true;
+    hideLoader();
+});
+
+// Imposta isVideoEnded a true quando il video è terminato
+document.getElementById('video_caricamento').addEventListener('ended', function() {
+    isVideoEnded = true;
+    hideLoader();
+});
+
+// Funzione per nascondere il loader se entrambe le condizioni sono vere
+function hideLoader() {
+    if (isPageLoaded && isVideoEnded) {
         document.getElementById('schermata_caricamento').style.display = 'none';
-        }
-    );
+    }
+}
