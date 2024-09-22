8d1f5d68f1cc2b363bf83117e8fa83c8fce2ff39
alfierilucapio <alfierilucapio@gmail.com>
2024-09-22 18:47:51 +0200

    correzione

--- assets/js/ScrollReveal.js
+++ assets/js/ScrollReveal.js
@@ -1,6 +1,6 @@
 ScrollReveal({distance:'100px',delay:500,duration:'2000',easing:'cubic-bezier(.215, .61, .355, 1)'});
 
-ScrollReveal().reveal('.reveal-zoom',{scale:1.2, reset:true})
+ScrollReveal().reveal('.reveal-zoom',{scale:1.2, reset:'true'})
 ScrollReveal().reveal('.reveal-left',{origin:'left',reset:'true'});
 ScrollReveal().reveal('.reveal-right',{origin:'right',reset:'true'});
 ScrollReveal().reveal('.reveal-top',{origin:'top', reset:'true'});
