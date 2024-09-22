f6d6dd7faf5c794fb960cb0920a34374a42b34d1
alfierilucapio <alfierilucapio@gmail.com>
2024-09-22 18:32:49 +0200

    Modifiche animazioni

--- assets/js/ScrollReveal.js
+++ assets/js/ScrollReveal.js
@@ -1,5 +1,13 @@
 ScrollReveal({distance:'100px'},{delay:500},{duration:'2000'},{easing:'cubic-bezier(.215, .61, .355, 1)'});
-ScrollReveal().reveal('.reveal-left',{origin:'left',reset:'true'});
-ScrollReveal().reveal('.reveal-right',{origin:'right',reset:'true'});
-ScrollReveal().reveal('.reveal-top',{origin:'top', reset:'true'});
-ScrollReveal().reveal('.reveal-bottom',{origin:'bottom', reset:'true'});
+ScrollReveal().reveal('.reveal-left-pc',{origin:'left',reset:'true'});
+ScrollReveal().reveal('.reveal-right-pc',{origin:'right',reset:'true'});
+ScrollReveal().reveal('.reveal-top-pc',{origin:'top', reset:'true'});
+ScrollReveal().reveal('.reveal-bottom-pc',{origin:'bottom', reset:'true'});
+ScrollReveal().reveal('.reveal-left-mobile',{origin:'left', reset:'true', desktop:'false'});
+ScrollReveal().reveal('.reveal-right-mobile',{origin:'right', reset:'true', desktop:'false'});
+ScrollReveal().reveal('.reveal-top-mobile',{origin:'top', reset:'true', desktop:'false'});
+ScrollReveal().reveal('.reveal-bottom-mobile',{origin:'bottom', reset:'true', desktop:'false'});
+ScrollReveal().reveal('.reveal-left-pc',{origin:'left',reset:'true', mobile:'false'});
+ScrollReveal().reveal('.reveal-right-pc',{origin:'right',reset:'true', mobile:'false'});
+ScrollReveal().reveal('.reveal-top-pc',{origin:'top', reset:'true', mobile:'false'});
+ScrollReveal().reveal('.reveal-bottom-pc',{origin:'bottom', reset:'true', mobile:'false'});
