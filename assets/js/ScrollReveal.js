ef75b07641403256737d5c47e639537480923656
alfierilucapio <146241829+alfierilucapio@users.noreply.github.com>
2024-09-22 05:49:57 +0200

    Update ScrollReveal.js

--- assets/js/ScrollReveal.js
+++ assets/js/ScrollReveal.js
@@ -1,4 +1,4 @@
-ScrollReveal({distance:'100px'},{delay:500},{duration:'1100'},{easing:'cubic-bezier(.215, .61, .355, 1)'});
+ScrollReveal({distance:'100px'},{delay:500},{duration:'2000'},{easing:'cubic-bezier(.215, .61, .355, 1)'});
 ScrollReveal().reveal('.reveal-left',{origin:'left',reset:'true'});
 ScrollReveal().reveal('.reveal-right',{origin:'right',reset:'true'});
 ScrollReveal().reveal('.reveal-top',{origin:'top', reset:'true'});
