b3d8cc3bc5b3fd49ba368ae59bbf2b09f0a73fcc
alfierilucapio <146241829+alfierilucapio@users.noreply.github.com>
2024-09-23 02:51:15 +0200

    Update Caricamento.js

--- assets/js/Caricamento.js
+++
@@ -1,7 +0,0 @@
-const video = document.getElementById('video_caricamento');
-const mainContent = document.getElementById('main-content');
-
-video.addEventListener('ended', () => {
-  mainContent.style.display = 'block';
-  video.style.display = 'none';
-});
