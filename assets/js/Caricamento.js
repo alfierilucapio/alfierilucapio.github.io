f12764a5918b2163fe135a50082579c0dce47ae7
alfierilucapio <alfierilucapio@gmail.com>
2024-09-23 21:19:20 +0200

    Modifiche gif

--- assets/js/Caricamento.js
+++ assets/js/Caricamento.js
@@ -17,7 +17,7 @@ if(videoEnded&&pageLoaded==true){
 } */
 
 // Durata della GIF in millisecondi (esempio: 5 secondi)
-const gifDuration = 2866.667;
+const gifDuration = 2580;
 let pageLoaded = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -33,7 +33,7 @@ function onGifEnd() {
             div.style.display = 'none';
         }, 500); // Tempo di transizione in millisecondi
     }else{
-        gifDuration = gifDuration + 2866.667;
+        gifDuration = gifDuration + 2580;
     }
 }
 
