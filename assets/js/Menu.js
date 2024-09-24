46b32f1c613871195c2b046880bee6c96e0f77fa
alfierilucapio <146241829+alfierilucapio@users.noreply.github.com>
2024-09-24 03:24:16 +0200

    Update Menu.js

--- assets/js/Menu.js
+++ assets/js/Menu.js
@@ -4,10 +4,13 @@ let isOpen = false;
 
 function mostraDiv() {
     let isCountEven = count % 2 === 0;
+    
+    let portfolio = document.getElementById('portfolio');
     let div = document.getElementById('mioDiv');
 
     if (isCountEven) {
         div.style.display = 'block';
+        portfolio.style.display = 'none';
     }
 
     div.classList.remove("animazione");
@@ -26,6 +29,7 @@ function mostraDiv() {
         div.style.opacity = "0";
         div.style.pointerEvents = "none";
         
+        portfolio.style.display = 'block';
         setTimeout(() => {
             div.style.display = 'none';
         }, 500); // Imposta un timeout per nascondere il div dopo l'animazione
